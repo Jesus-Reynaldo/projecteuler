a0b485604ecd61c66af9f1024970e5da02a9e459
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-06-17 23:59:45 -0400

    Factorial digit sum

--- Problem20.js
+++ Problem20.js
@@ -1,14 +1,14 @@
 const factorialSum=(num)=>{
-    let factorial=1;
+    let factorial=BigInt(1);
     let sum=BigInt(0);
-    for(let i =1;i<num;i++){
-        factorial*=i;
+    for(let i =BigInt(1);i<num;i++){
+        factorial=BigInt(factorial*i);
     }
     let number= BigInt(factorial);
     do{
         sum+=BigInt(number%10n);
         number=BigInt(number/10n);
-    }while(factorial!=0);
+    }while(number!=0);
     return sum;
 }
 console.log(factorialSum(100));
