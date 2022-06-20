dcd1faea5545c960ea97c8228fdf8b1e91d96a21
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-06-20 16:52:14 -0400

    Power digit sum Optimum

--- Problem16.js
+++ Problem16.js
@@ -1,12 +1,12 @@
 /*2^15 = 32768 y la suma de sus dígitos es 3 + 2 + 7 + 6 + 8 = 26.
 ¿Cuál es la suma de los dígitos del número 2^1000? */
 const powerDigitSum=(number,power)=>{
-    let pow=BigInt(Math.pow(number,power));
-    let sum=BigInt(0);
-    do{
-        sum+=BigInt(pow%10n);
-        pow=BigInt(pow/10n);
-    }while(pow!=0);
+    let pow=BigInt(Math.pow(number,power)).toString();
+    let len=pow.length;
+    let sum=0;
+    for (let i=0; i<len; i++){
+        sum+=parseInt(pow[i]);
+    }
     return sum;
 }
 console.log(powerDigitSum(2,1000));
